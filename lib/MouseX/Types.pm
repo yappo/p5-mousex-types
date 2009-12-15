f8ced4a56ca5ef6d610a7ddcadac0dd394ed0d24
gfx <gfuji@cpan.org>
2009-12-15 12:12:43 +0900

    Checking in changes prior to tagging of version 0.02_02.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 8e6a19f..242e380 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension MouseX::Types
    
    +0.02_02 Tue Dec 15 12:05:34 2009
    +    - Fix parametarization issue (gfx)
    +
     0.02_01 Sun Dec 13 16:39:09 2009
         - Refactoring for new Mouse (gfx)
         - Fix import to warn about undefined types (gfx)

--- lib/MouseX/Types.pm
+++ lib/MouseX/Types.pm
@@ -2,7 +2,7 @@ package MouseX::Types;
 use 5.006_002;
 use Mouse::Exporter; # turns on strict and warnings
 
-our $VERSION = '0.02_01';
+our $VERSION = '0.02_02';
 
 use Mouse::Util::TypeConstraints ();
 
