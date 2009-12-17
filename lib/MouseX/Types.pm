6f0ab459f8849a9c8312961afc0368e6641852d9
gfx <gfuji@cpan.org>
2009-12-17 14:17:44 +0900

    Checking in changes prior to tagging of version 0.02_03.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 242e380..ad0fdbb 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension MouseX::Types
    
    +0.02_03 Thu Dec 17 14:16:53 2009
    +    - Fix segmentation fault issue on older perls (gfx)
    +
     0.02_02 Tue Dec 15 12:05:34 2009
         - Fix parametarization issue (gfx)

--- lib/MouseX/Types.pm
+++ lib/MouseX/Types.pm
@@ -2,7 +2,7 @@ package MouseX::Types;
 use 5.006_002;
 use Mouse::Exporter; # turns on strict and warnings
 
-our $VERSION = '0.02_02';
+our $VERSION = '0.02_03';
 
 use Mouse::Util::TypeConstraints ();
 
