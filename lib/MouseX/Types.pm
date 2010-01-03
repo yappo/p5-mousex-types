5843ca165299bc993a4254c46936d9ab6cad5675
gfx <g.psy.va@gmail.com>
2010-01-03 10:30:15 +0900

    Checking in changes prior to tagging of version 0.04.  Changelog diff is:
    diff --git a/Changes b/Changes
    index c1d6a23..f89b94c 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension MouseX::Types
    
    +0.04 Sun Jan  3 10:23:47 2010
    +    - Add copyright information (RT #53013)
    +    - Support type predicates
    +        (e.g. "use MouseX::Types::Mouse qw(is_Int)")
    +
     0.03 Sun Dec 20 15:13:39 2009
         - Shipped as a stable version

--- lib/MouseX/Types.pm
+++ lib/MouseX/Types.pm
@@ -2,7 +2,7 @@ package MouseX::Types;
 use 5.006_002;
 use Mouse::Exporter; # turns on strict and warnings
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Mouse::Util::TypeConstraints ();
 
